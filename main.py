49808acd10d3ce42d1849a2898a0ee6c418c852a
RaulVS <raul.valls@aquilessolutions.com>
2021-10-18 17:22:38 +0200

    Read LDLC products correctly

--- main.py
+++ main.py
@@ -2,23 +2,47 @@
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from model import GPU
 import pandas as pd
+import unicodedata
 
-driver = webdriver.Chrome("C:\\chromedriver.exe")
-#driver = webdriver.Chrome("C:\Program Files\Google\Chrome\Application")
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
-#driver.get("<a href="https://www.flipkart.com/laptops/">https://www.flipkart.com/laptops/</a>~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;amp;amp;amp;amp;amp;amp;uniq")
-driver.get("https://www.ldlc.com/es-es/informatica/piezas-de-informatica/tarjeta-grafica/c4684/+fdi-1+fv1766-9941,16762.html?sort=1")
+def normalizePrice(readPrice):
+    return readPrice.strip().replace('€', ',').replace(' ','')
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser" )
-for a in soup.findAll('li', attrs={'class':'pdt-item'}):
-    name=a.find('div', attrs={'class':'_3wU53n'})
-    price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-    rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-    products.append(name.text)
-    prices.append(price.text)
-    ratings.append(rating.text)
+def removeHtmlSpecials(text):
+    return unicodedata.normalize("NFKD", text)
+
+def main():
+    gpu = []
+    driver = webdriver.Chrome("C:\\chromedriver.exe")
+    driver.get("https://www.ldlc.com/es-es/informatica/piezas-de-informatica/tarjeta-grafica/c4684/+fdi-1+fv1766-9941,16762.html?sort=1")
+
+    content = driver.page_source
+    soup = BeautifulSoup(content, "html.parser", from_encoding="utf-8")
+    for prod in soup.findAll('li', attrs={'class':'pdt-item'}):
+        name=removeHtmlSpecials(prod.find('h3', attrs={'class':'title-3'}).find('a').getText())
+        stock=removeHtmlSpecials(prod.find('div', attrs={'class':'stock'}).getText())
+        price=normalizePrice(removeHtmlSpecials(prod.find('div', attrs={'class':'price'}).getText()))
+        print(name ," -- ", stock ," -- ", price)
+    
+
+if __name__ == "__main__":
+    main()
+
+
+# print(
+#     "Found product:\n"
+#     "Name: " +name +"\n"
+#     "Price: " +price +"\n"
+#     "Stock: " +stock +"\n"
+# )
+
+
+#<div class="price">1&nbsp;700€<sup>95</sup></div>
+#<div class="modal-stock-web pointer stock stock-1" data-stock-web="1"><span>En <em>stock</em></span>
+# price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+# rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
+# products.append(name.text)
+# prices.append(price.text)
+# ratings.append(rating.text)
---
+++ model/Product.py
@@ -0,0 +1,8 @@
+class Product:
+    def __init__(self, name, price, stock):
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+    def toString():
+        return "Product to string"
